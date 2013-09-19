662078fd8916680113f14e7da4800e949cd6e301
Remy Sharp <remy@remysharp.com>
2013-09-19 21:25:33 +0200

    add /referer support

--- index.js
+++ index.js
@@ -3,16 +3,17 @@ var request = require('request'),
     cors = require('cors'),
     cheerio = require('cheerio'),
     parse = require('url').parse,
+    path = require('path'),
     prepare = require('./public/prepare');
 
 function proxy(req, res, next) {
   var url = parse(req.url, true);
 
   if (url.pathname === '/proxy') {
-    var path = url.query.url;
-    var base = path.split('/').slice(0, -1).join('/') + '/';
+    var url = url.query.url;
+    var base = path.normalize(url) + '/';
 
-    request(path, function (error, response, body) {
+    request(url, function (error, response, body) {
       if (!error && response.statusCode == 200) {
         if (body.toUpperCase().indexOf('<BODY') === -1) { // damn bodyless things...
           body = '<body>' + body + '</body>';
@@ -32,6 +33,16 @@ function proxy(req, res, next) {
 }
 
 connect()
+  .use(function (req, res, next) {
+    console.log(req.headers);
+    if (req.url === '/referer' && req.headers.referer) {
+      res.writeHead(302, { location: '/proxy?url=' + req.headers.referer });
+      res.end();
+    } else {
+      // TODO send a explanation page
+    }
+    next();
+  })
   .use(connect.static('public'))
   // TODO add cors
   .use(proxy)
