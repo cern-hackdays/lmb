92f8d813346f8af5d9c2118e2d927f2407e4ff88
Remy Sharp <remy@remysharp.com>
2013-09-19 12:13:12 +0200

    just might actually commit the file...

---
+++ index.js
@@ -0,0 +1,21 @@
+var request = require('request'),
+    connect = require('connect'),
+    parse = require('url').parse;
+
+function proxy(req, res, next) {
+  var url = parse(req.url, true);
+
+  if (url.pathname === '/proxy') {
+    var x = request(url.query.url);
+    req.pipe(x);
+    x.pipe(res);
+  } else {
+    next();
+  }
+}
+
+connect()
+  .use(connect.static('public'))
+  // TODO add cors
+  .use(proxy)
+  .listen(process.env.PORT || 8000);
