e99b0bc9032fa0ad6458341b7a97d94a7ee0de29
Remy Sharp <remy@remysharp.com>
2013-09-20 00:21:33 +0200

    Initial version of using canvas to reveal each char
    Still feels slow, and alignment is utterly off, but this feels like a CSS issue - or certainly a mapping pixels to screen type issue.
    
    Maybe just clear a line at a time instead?

--- public/blocker.js
+++ public/blocker.js
@@ -1,24 +1,55 @@
 var position = 0,
-    char = [0,0];
+    length = 80 * 24,
+    char = [0,0],
+    ctx,
+    run = true;
 
 function init() {
-  var ctx = document.createElement('canvas').getContext('2d'),
-      canvas = ctx.canvas;
+  var canvas = document.createElement('canvas');
+  ctx = canvas.getContext('2d');
 
   canvas.id = 'blocker';
   document.body.appendChild(canvas);
+  canvas.width = 80 * 8; // 8 is the character width
+  canvas.height = 24 * 24; // 14px character height
+
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  // TODO handle resizing
 }
 
 function exit() {
+  ctx.canvas.parentNode.removeChild(ctx.canvas);
+  run = false;
+}
 
+function getCords(position) {
+  var x = position % 80,
+      y = position / 80 | 0;
+
+  return {
+    x: x,
+    y: y
+  };
 }
 
 function draw() {
-  requestAnimationFrame(draw);
+  if (run) requestAnimationFrame(draw);
+
+  var coords = getCords(position);
+
+  console.log(coords);
+
+  ctx.clearRect(coords.x * 80, coords.y * 24, 80, 24);
+
+  position++;
+
+  if (position >= length) exit();
 }
 
 function moveto() {
 
 }
 
-// draw();
+init();
+draw();
