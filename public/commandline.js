d75de995ec804829bdab036f6e1f5b2eb96473c4
Remy Sharp <remy@remysharp.com>
2013-09-19 15:43:17 +0200

    console

--- public/commandline.js
+++ public/commandline.js
@@ -1,1 +1,14 @@
-// hi
+var cmd = document.querySelector('#command input');
+
+cmd.onkeydown = function (e) {
+  e.preventDefault();
+  console.log(e);
+  if (e.keyCode === 13) {
+    run(this.value);
+    this.value = '';
+  }
+};
+
+function run(command) {
+  console.log(command);
+}
