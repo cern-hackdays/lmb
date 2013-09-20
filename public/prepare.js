817bb6b95f2fbca3f70280aeddb332d1d8756208
Brian LeRoux <brian@westcoastlogic.com>
2013-09-20 12:28:20 +0200

    deep linking should work

--- public/prepare.js
+++ public/prepare.js
@@ -1,10 +1,13 @@
-var commandline = ['<form id=command><label><span class="cmd-prompt">&lt;ref.number&gt;, Quit, or Help: </span><span autofocus autocapitalize=off spellcheck=false autocorrect=off contenteditable id="cmd-input"></span><span class=wait id="cmd-cursor">&nbsp;</span><input hidden autofocus></label></form>',
-  '<script src=blocker.js></script>',
-  '<script src=keyboardSounds.js></script>',
-  '<script src=commandline.js></script>',].join('');
+module.exports = function prepare ($, location) {
 
-module.exports = function ($, location) {
-  
+  var commandline = ['<form id=command><label><span class=cmd-prompt>&lt;ref.number&gt;, Quit, or Help: </span>',
+                     '<span autofocus autocapitalize=off spellcheck=false autocorrect=off contenteditable id=cmd-input></span>',
+                     '<span class=wait id=cmd-cursor>&nbsp;</span><input hidden autofocus></label></form>',
+                     '<script src=blocker.js></script>',
+                     '<script src=keyboardSounds.js></script>',
+                     '<script src=commandline.js></script>'].join('');
+
+
   // strip particular elements
   $('style,iframe,frame,frameset,img,hr,br').remove();
   $('link[rel=stylesheet]').remove();
@@ -21,7 +24,7 @@ module.exports = function ($, location) {
   
   // all documents have an [END] at the...
   // TODO parser question?
-  $('body').append('<pre id="lmb-footer">\n\n     [End]</pre>');
+  $('body').append('<pre id=lmb-footer>\n\n     [End]</pre>');
 
   
   // insert command prompt
@@ -38,23 +41,24 @@ module.exports = function ($, location) {
     $el.append('[' + (i+1) + ']');
 
     var href = $el.attr('href');
-    href = 'about'
+    //href = '/about/foo/foobar'
     var url = require('querystring').parse(location)['/proxy?url'] // brian.io/whatever/whatevers
     var host = require('url').parse(url).hostname                  // brian.io
     var protocol = require('url').parse(url).protocol
 
+    // console.log(href)
 
+    // FIXME this is the href munging code. should be own, tested, module.
     // order important here
     if (href === '/') href = protocol + '//' + host    
     if (href.toString().slice(0, 2) === '//') href = protocol + href;
     if (href.slice(0, 1) === '/') href = protocol + '//' +  host + href
-    // FIXME this is brittle and will break on deep links
     if (href.slice(0, protocol.length) != protocol) href = protocol + '//' + host + '/' +  href 
     
     // final rewrite to proxy url
     href = '/proxy?url=' + href
 
-
+    // console.log(href)
 
     // set the proper rewritten url
     $el.attr('href', href);
