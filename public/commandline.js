46392370086a644b9d29815b37baffcec8b4d494
Remy Sharp <remy@remysharp.com>
2013-09-20 10:36:54 +0200

    send resize to blocker

--- public/commandline.js
+++ public/commandline.js
@@ -160,8 +160,10 @@ window.onload = function () {
 
 // Make sure 24 lines fit on the viewport and make the font-size as large as possible for that
 (window.adjustFontSize = function (){
-	var maxLineHeight = innerHeight / 25;
-	document.documentElement.style.fontSize = Math.floor(maxLineHeight / 1.5)  + 'px';
+	var maxLineHeight = innerHeight / 25,
+      size = Math.floor(maxLineHeight / 1.5);
+	document.documentElement.style.fontSize = size  + 'px';
+  blocker.size(size);
 })();
 
 addEventListener('resize', adjustFontSize);
