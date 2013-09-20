507349639154b35512a4b9faa08f886e784cc5dd
Kimberly Blessing <kimberly@kimberlyblessing.com>
2013-09-20 04:49:51 -0400

    Moved command prompt logic to commandline.js in order to match LMB display logic

--- public/prepare.js
+++ public/prepare.js
@@ -1,6 +1,6 @@
 (function () {
 
-var commandline = ['<form id=command><label><span class="cmd-prompt">&lt;ref.number&gt;, Quit, or Help: </span><input autofocus autocapitalize=off spellcheck=false autocorrect=off id="cmd-input"><span class=wait id="cmd-cursor">&nbsp;</span></label></form>',
+var commandline = ['<form id=command><label><span class="cmd-prompt"></span><span autofocus autocapitalize=off spellcheck=false autocorrect=off contenteditable id="cmd-input"></span><span class=wait id="cmd-cursor">&nbsp;</span><input hidden autofocus></label></form>',
   '<script src=blocker.js></script>',
   '<script src=keyboardSounds.js></script>',
   '<script src=commandline.js></script>',].join('');
